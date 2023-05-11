ce6338381033b467249cac3ed163761dabb94380
Luuk Tap <luuktap@gmail.com>
2023-05-11 12:52:37 +0200

    add trip controller with optimal trip endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import { router as tripsRouter } from './tripController';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
+app.use('/trips', tripsRouter);
+
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: 'NS Wrapper API' });
 });
 
 app.listen(port, host, () => {
---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import { Station, Trip } from './types';
+
+export const findOptimalTrip = (trips: Trip[]): Trip => {
+  const optimalTrip = trips.find((trip) => trip.optimal);
+
+  if (!optimalTrip) {
+    throw new Error('No optimal trip found');
+  }
+
+  return optimalTrip;
+};
+
+export const findStationByLongName = (
+  longName: string,
+  stations: Station[]
+): Station => {
+  const station = stations.find((station) => station.namen.lang === longName);
+
+  if (!station) {
+    throw new Error('Station not found');
+  }
+
+  return station;
+};
